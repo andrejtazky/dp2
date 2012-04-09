afa735458e15b9eb03e7e04a1d6527922ed650fd
Andrej Tazky <ayo@ynet.sk>
2012-04-09 16:32:19 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dp2::Application.config.secret_token = '85690616091d95f68051f55f606458b58e8d8a13cc56e5eba571d510db66adb4db82cd08f28fa7c9bb0faee60758df67ed1221c766d07cd3ba1200d94b40c1de'
